bfc10a8ccebaa6266e9f48facece243748b20bd4
Will Shown <w@willshown.com>
2018-12-13 18:11:49 -0600

    Add JSS

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import injectSheet from 'react-jss'
+import Intro from './pages/Intro'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const styles = {
+  app: {
+  },
 }
 
-export default App;
+const App = ({classes}) => <Router>
+  <main className={classes.app}>
+    <Route exact path="/" component={Intro} />
+  </main>
+</Router>
+
+export default injectSheet(styles)(App);
---
+++ src/style/colors.js
@@ -0,0 +1,4 @@
+export default {
+  dark: 'rgb(53,68,72)',
+  light: 'rgb(211,220,227)',
+}
