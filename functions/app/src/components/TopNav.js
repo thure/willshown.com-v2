2e2b08043f8f279a6d566afcdd54ea9fb8dbdc94
Will Shown <w@willshown.com>
2018-12-20 21:22:54 -0600

    Adjust TopNav

--- functions/app/src/components/TopNav.js
+++ functions/app/src/components/TopNav.js
@@ -3,6 +3,7 @@ import Headroom from 'react-headroom'
 import { Link } from 'react-router-dom'
 import injectSheet from 'react-jss'
 import { colors, shadows } from '../style'
+import cx from 'classnames'
 import { ReactComponent as PackageIcon } from 'feather-icons/dist/icons/package.svg'
 import { ReactComponent as LayersIcon } from 'feather-icons/dist/icons/layers.svg'
 import { ReactComponent as MenuIcon } from 'feather-icons/dist/icons/menu.svg'
@@ -11,6 +12,14 @@ import { ReactComponent as MarkIcon } from '../assets/mark.svg'
 const styles = {
   outerContainer: {
     paddingBottom: '3px',
+    transition: 'transform 120ms ease-in-out, opacity 0ms linear 0ms',
+    transform: 'translate3d(0, 0, 0)',
+    opacity: 1,
+  },
+  hiddenOuterContainer: {
+    transition: 'transform 120ms ease-in-out, opacity 0ms linear 120ms',
+    transform: 'translate3d(0, -100%, 0)',
+    opacity: 0,
   },
   innerContainer: {
     display: 'flex',
@@ -28,6 +37,11 @@ const styles = {
     width: '3rem',
     height: '3rem',
     color: 'white',
+    transition: 'color 120ms linear',
+    overflow: 'hidden',
+    '&:hover': {
+      color: colors.red,
+    },
   },
   topNavHomeLink: {
     color: colors.red,
@@ -41,10 +55,6 @@ const styles = {
   topNavLinkContent: {
     display: 'block',
     width: '100%',
-    transition: 'color 120ms linear',
-    '&:hover': {
-      color: colors.red,
-    },
   },
   topNavLinkIcon: {
     display: 'block',
@@ -58,15 +68,18 @@ const styles = {
     width: '100%',
     height: 'auto',
     margin: '0 auto',
+    transform: 'translate(1px, -1px)',
   },
 }
 
 const TopNavLink = injectSheet(styles)(({ children, to, classes }) => (
   <Link
     to={to}
-    className={`topNavLink ${classes.topNavLink}${
-      to === '/' ? ` ${classes.topNavHomeLink}` : ''
-    }`}
+    className={cx(
+      'topNavLink',
+      classes.topNavLink,
+      to === '/' && classes.topNavHomeLink
+    )}
   >
     <div className={classes.topNavLinkContent}>{children}</div>
   </Link>
@@ -74,7 +87,12 @@ const TopNavLink = injectSheet(styles)(({ children, to, classes }) => (
 
 const TopNav = ({ isAuthenticated, pathName, classes }) => (
   <Headroom>
-    <div className={classes.outerContainer}>
+    <div
+      className={cx(
+        classes.outerContainer,
+        pathName === '/' && classes.hiddenOuterContainer
+      )}
+    >
       <nav className={classes.innerContainer}>
         <TopNavLink to="/">
           <MarkIcon className={classes.topNavHomeIcon} />
