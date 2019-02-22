02d8ce4922ffc4cf6bec99525102223b67b4e15b
Will Shown <w@willshown.com>
2019-02-22 16:36:41 -0500

    Can filter skills

--- functions/app/src/components/SkillCloud.js
+++ functions/app/src/components/SkillCloud.js
@@ -1,18 +1,24 @@
 import React from 'react'
+import cx from 'classnames'
 
-import { colors, typeScale } from '../style'
+import { colors, fonts, typeScale, icons } from '../style'
 
 import Paper from '@material-ui/core/Paper'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
+import InputBase from '@material-ui/core/InputBase'
 import { withStyles } from '@material-ui/core'
 
-const styles = {
-  container: {
+const styles = theme => ({
+  skillsContainer: {
     display: 'flex',
     flexFlow: 'row wrap',
     marginRight: '-.6rem',
     marginBottom: '1rem',
+  },
+  filterContainer: {
+    paddingTop: '.4rem',
+    paddingBottom: '.6rem',
   },
   skill: {
     display: 'flex',
@@ -21,6 +27,11 @@ const styles = {
     background: 'white',
     margin: '0 .6rem .6rem 0',
     overflow: 'hidden',
+    order: 0,
+  },
+  skillHidden: {
+    visibility: 'hidden',
+    order: 1,
   },
   skillTitle: {
     padding: '.3rem .5rem .3rem .6rem',
@@ -50,39 +61,111 @@ const styles = {
     position: 'relative',
     zIndex: 1,
   },
+  filter: {
+    padding: '.25rem',
+    display: 'flex',
+    alignItems: 'center',
+    [theme.breakpoints.up('sm')]: {
+      maxWidth: '18rem',
+    },
+  },
+  filterInput: {
+    display: 'block',
+    marginLeft: '.5rem',
+    marginRight: '.5rem',
+    flex: '1 1 auto',
+    ...typeScale(0),
+    ...fonts.raleway.medium,
+  },
+  filterIcon: {
+    display: 'block',
+    height: '1.2rem',
+    width: '1.2rem',
+    strokeWidth: ['1.64', '!important'],
+    marginRight: '.4rem',
+  },
+})
+
+const skillShouldHide = (query, skill) => {
+  if (query.length < 2) return false
+  if (skill.title.toLowerCase().includes(query)) return false
+  if (skill.keywords)
+    for (let i = 0; i < skill.keywords.length; i++) {
+      if (skill.keywords[i].toLowerCase().includes(query)) return false
+    }
+  return true
 }
 
-const Skill = ({ skill, classes }) => (
-  <Paper elevation={1} className={classes.skill}>
-    <Typography variant="button" className={classes.skillTitle} component="div">
-      {skill.title}
-    </Typography>
-    <Typography variant="button" className={classes.years} component="div">
-      <i
-        className={classes.skillShade}
-        style={{
-          opacity:
-            (Math.log(parseInt(skill.years) / 8) / 2.5 + 0.8).toFixed(3) || 0,
-        }}
-      />
-      <span className={classes.skillYears}>{skill.years}</span>
-    </Typography>
-  </Paper>
-)
+const Skill = ({ query, skill, classes }) => {
+  const shade =
+    (Math.log(parseInt(skill.years) / 8) / 2.5 + 0.8).toFixed(3) || 0
+  const hide = skillShouldHide(query, skill)
+  return (
+    <Paper
+      elevation={1}
+      className={cx(classes.skill, hide && classes.skillHidden)}
+    >
+      <Typography
+        variant="button"
+        className={classes.skillTitle}
+        component="div"
+      >
+        {skill.title}
+      </Typography>
+      <Typography variant="button" className={classes.years} component="div">
+        <i className={classes.skillShade} style={{ opacity: shade }} />
+        <span className={classes.skillYears}>{skill.years}</span>
+      </Typography>
+    </Paper>
+  )
+}
 
 class SkillCloud extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      inputValue: '',
+    }
+
+    this.handleInputChange = this._handleInputChange.bind(this)
   }
+
+  _handleInputChange(event) {
+    this.setState({ inputValue: event.target.value })
+  }
+
   render() {
     const { skills, classes } = this.props
+    const { inputValue } = this.state
 
     return (
-      <CardContent className={classes.container}>
-        {skills.map((skill, si) => (
-          <Skill skill={skill} classes={classes} key={`skill__${si}`} />
-        ))}
-      </CardContent>
+      <React.Fragment>
+        <CardContent className={classes.filterContainer}>
+          <Paper elevation={1} className={classes.filter}>
+            <InputBase
+              className={classes.filterInput}
+              placeholder="Filter by name or keyword"
+              onChange={this.handleInputChange}
+              value={inputValue}
+              spellCheck={false}
+              type="search"
+              results={0}
+            />
+            <icons.SearchIcon className={classes.filterIcon} />
+          </Paper>
+        </CardContent>
+        <CardContent className={classes.skillsContainer}>
+          {skills.map((skill, si) => (
+            <Skill
+              skill={skill}
+              classes={classes}
+              query={inputValue.toLowerCase()}
+              key={`skill__${si}`}
+            />
+          ))}
+        </CardContent>
+      </React.Fragment>
     )
   }
 }
--- functions/app/src/style/icons.js
+++ functions/app/src/style/icons.js
@@ -232,6 +232,24 @@ const ChevronRight = ({ className }) => (
   </svg>
 )
 
+const SearchIcon = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+  >
+    <circle cx="11" cy="11" r="8" />
+    <line x1="21" y1="21" x2="16.65" y2="16.65" />
+  </svg>
+)
+
 export {
   ArrowLeftIcon,
   PackageIcon,
@@ -245,4 +263,5 @@ export {
   DownloadIcon,
   ChevronLeft,
   ChevronRight,
+  SearchIcon,
 }
